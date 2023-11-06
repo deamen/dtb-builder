dc0209c13c148f6d6ab3a6328bfa8b3db4ac303b
Song Tang <stang@mmz.au>
2023-11-06 13:07:12 +1100

    Add orangepi_zero3

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # dtb-builder
 Build mainline dtb files with Buildah and Qemu User Emulator
+
+# How to build dtb for Orange Pi Zero3
+```bash
+./orangepi_zero3/build_dtb_orangepi_zero3.sh
+```
---
+++ orangepi_zero3/build_dtb_orangepi_zero3.sh
@@ -0,0 +1,31 @@
+
+# Set kernel_ver environment variables
+export kernel_ver="v6.6"
+
+export container=$(buildah from arm64v8/alpine:3.18)
+buildah config --label maintainer=""github.com/deamen"" $container
+
+# Set kernel_ver environment variables in container
+buildah config --env kernel_ver=$kernel_ver $container
+
+buildah run $container apk add git gcc make libc-dev bison flex openssl-dev python3 dtc gcc-arm-none-eabi py3-setuptools swig python3-dev py3-elftools patch
+buildah run $container git clone https://github.com/torvalds/linux.git --depth 1 --branch $kernel_ver
+
+buildah copy $container orangepi_zero3/0001-orangepi-zero3-dts-linux-6.6.patch /tmp/
+
+buildah config --workingdir "/linux" $container
+buildah run $container git apply /tmp/0001-orangepi-zero3-dts-linux-6.6.patch
+buildah run $container make defconfig
+buildah run $container make -j$(nproc --ignore 1) dtbs
+
+copy_script="copy_artifacts.sh"
+cat << 'EOF' >> $copy_script
+#!/bin/sh
+mnt=$(buildah mount $container)
+cp $mnt/$1 ./out/$2
+buildah umount $container
+EOF
+chmod a+x $copy_script
+buildah unshare ./$copy_script linux/arch/arm64/boot/dts/allwinner/sun50i-h618-orangepi-zero3.dtb dtb_orangepi_zero3.dtb
+rm ./$copy_script
+buildah rm $container
