c4f8e6119111e7c5e261acd4f2319d12ee828417
Song Tang <stang@mmz.au>
2023-11-06 20:25:00 +1100

    Add nanopi-r4s DTB

---
+++ nanopi-r4s/build_dtb_nanopi-r4s.sh
@@ -0,0 +1,28 @@
+
+# Set kernel_ver environment variables
+export kernel_ver="v6.6"
+
+export container=$(buildah from arm64v8/alpine:3.18)
+buildah config --label maintainer=""github.com/deamen"" $container
+
+# Set kernel_ver environment variables in container
+buildah config --env kernel_ver=$kernel_ver $container
+
+buildah run $container apk add git gcc make libc-dev bison flex openssl-dev python3 dtc gcc-arm-none-eabi py3-setuptools swig python3-dev py3-elftools patch
+buildah run $container git clone https://github.com/torvalds/linux.git --depth 1 --branch $kernel_ver
+
+buildah config --workingdir "/linux" $container
+buildah run $container make -j$(nproc --ignore 1) defconfig
+buildah run $container make -j$(nproc --ignore 1) rockchip/rk3399-nanopi-r4s.dtb
+
+copy_script="copy_artifacts.sh"
+cat << 'EOF' >> $copy_script
+#!/bin/sh
+mnt=$(buildah mount $container)
+cp $mnt/$1 ./out/$2
+buildah umount $container
+EOF
+chmod a+x $copy_script
+buildah unshare ./$copy_script linux/arch/arm64/boot/dts/rockchip/rk3399-nanopi-r4s.dtb dtb_nanopi-r4s.dtb
+rm ./$copy_script
+buildah rm $container
